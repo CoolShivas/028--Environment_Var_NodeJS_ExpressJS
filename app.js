661136e58c0857d217275a65df5f01d4b8035108
CoolShivas <shivas2710cool00@gmail.com>
2025-05-10 01:15:34 +0530

    Directly, use of .env file with npm run dev.

--- app.js
+++ app.js
@@ -39,8 +39,10 @@ app.listen(PORT, () => {
 });
 // // // Open the terminal and write down the following commands to run/start the server :-
 // // // Firstly, clear all the things and ls to check the folder;
-// // // Secondly, if you are using the Command Prompt Terminal then write the command :- set PORT=2050 && node --watch app.js to start the server;
-// // // Finally, your server started :- Server running at 2050 
-// // // Now, open the browser and in url write localhost:2050
+// // // Secondly, without using Powershell and Command Prompt Terminal to start the server;
+// // // Thirdly, create .env file and add dev to package.json (Have a look on both the files);
+// // // Fourthly, write the command on Powershell Terminal as npm run dev 
+// // // Finally, your server started :- Server running at 1729
+// // // Now, open the browser and in url write localhost:1729
 // // // You will get the result on the display as :- Welcome to the Home Page.
-// // // As well as you can switch the pages as localhost:2050/about or localhost:2050/contact;
+// // // As well as you can switch the pages as localhost:1729/about or localhost:1729/contact;
