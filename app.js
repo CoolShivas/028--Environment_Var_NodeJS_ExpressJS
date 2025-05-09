9ed01fff80d1bb93381e05dfc329340e1fac65a0
CoolShivas <shivas2710cool00@gmail.com>
2025-05-10 00:26:05 +0530

    Formation of About and Contact Page.

--- app.js
+++ app.js
@@ -6,6 +6,29 @@ app.get("/", (request, response) => {
     response.send("Welcome to the Home Page.");
 });
 
+app.get("/about", (request, response) => {
+    response.send("Welcome to the About Page.");
+});
+
+app.get("/contact", (request, response) => {
+    return response.send(`<div class="container">
+        <h1>URL Shortener</h1>
+        <form id="shorten-form">
+            <div>
+                <label for="url">Enter URL :-</label>
+                <input type="url" name="url" id="url" required>
+            </div>
+            <div>
+                <label for="shortCode">Enter ShortCode :-</label>
+                <input type="text" name="shortCode" id="shortCode" required>
+            </div>
+            <button type="submit">Shorten</button>
+        </form>
+        <h2>Shortened URLs</h2>
+        <ul id="shortened-urls"></ul>
+    </div>`);
+});
+
 const PORT = 1234;
 
 app.listen(PORT, () => {
@@ -17,3 +40,4 @@ app.listen(PORT, () => {
 // // // Finally, your server started :- Server running at 1234;
 // // // Now, open the browser and in url write localhost:1234
 // // // You will get the result on the display as :- Welcome to the Home Page.
+// // // As well as you can switch the pages as localhost:1234/about or localhost:1234/contact;
