0094e3ba4bb36e0252cba4c30815ff6ee54f4acf
CoolShivas <shivas2710cool00@gmail.com>
2025-05-10 00:34:21 +0530

    Use of Environment Variable in Powershell Terminal.

--- app.js
+++ app.js
@@ -29,15 +29,18 @@ app.get("/contact", (request, response) => {
     </div>`);
 });
 
-const PORT = 1234;
+
+// // // Use of Environment Variable;
+// const PORT = 1234;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Server running at ${PORT}`);
 });
 // // // Open the terminal and write down the following commands to run/start the server :-
 // // // Firstly, clear all the things and ls to check the folder;
-// // // Secondly, node --watch app.js to start the server;
-// // // Finally, your server started :- Server running at 1234;
-// // // Now, open the browser and in url write localhost:1234
+// // // Secondly, if you are using the Powershell Terminal then write the command :- $env:PORT = 6789; node --watch app.js to start the server;
+// // // Finally, your server started :- Server running at 6789;
+// // // Now, open the browser and in url write localhost:6789
 // // // You will get the result on the display as :- Welcome to the Home Page.
-// // // As well as you can switch the pages as localhost:1234/about or localhost:1234/contact;
+// // // As well as you can switch the pages as localhost:6789/about or localhost:6789/contact;
